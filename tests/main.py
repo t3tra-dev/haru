59bc4243b1de864fbed24b8052c2ad8371b55a83
t3tra-dev <admin@t3tra.net>
2024-11-01 10:10:39 +0900

    fix: test

--- tests/main.py
+++ tests/main.py
@@ -1,28 +1,32 @@
-import logging
-
+import haru
 from haru import Haru, Request
-from haru.middlewares import LoggerMiddleware
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from haru.ui import Page, Div, Br
+from haru.ui.utils import Markdown
 
 app = Haru(__name__)
-app.add_middleware(LoggerMiddleware(logger=logger))
 
 
 @app.route("/")
 def index(req: Request):
-    return "Hello, world!"
+    return Page(
+        Div(
+            Markdown(
+                f"# Hello, world!\n\nWelcome to Haru/{haru.__version__}, the Python framework for web applications."
+            ),
+            Br(),
+            Div(f"request info: {req.method} {req.path} {req.headers}"),
+        )
+    )
 
 
 @app.route("/user/<username:str>")
 def user(req: Request):
-    return f'Hello, {req.params["username"]}!'
+    return Page(Markdown(f"# Hello, {req.params['username']}!"))
 
 
 @app.errorhandler(404)
 def not_found(req: Request, exc: Exception):
-    return "Not found.", 404
+    return Page(Markdown("# Not found.\n\nThe page you are looking for does not exist.")), 404
 
 
 app.run()
